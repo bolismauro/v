d0ec9eea35bdfff5d07f5394a9dc64de8435a984
Mauro Bolis <bolismauro@gmail.com>
2013-06-11 16:57:13 +0200

    added basic commit func

--- README.md
+++ README.md
@@ -18,4 +18,5 @@ v -M   # Increment Major version
 v -m   # Increment Minor version
 v -p   # Increment Patch number
 v --set 1.2.3 # Manually set version
+v -c #Insert commit message
 ```
--- index.js
+++ index.js
@@ -11,8 +11,11 @@
 var program = require('commander')
   , clog = require('clog')
   , fs = require('fs')
+  , version = require('./lib/version')
+  , git = require('gitty')
   , data
-  , version;
+  , version
+  , currentRepo;
 
 program
   .version('0.0.1')
@@ -20,43 +23,34 @@ program
   .option('-m, --minor', 'Auto increment minor version and reset patch')
   .option('-p, --patch', 'Auto increment patch')
   .option('-s, --set <M>.<m>.<p>', 'Manaully set version')
+  .option('-c, --message <message>', 'The commit message')
   .parse(process.argv);
 
+// update the version in the package.json
+// TODO: remove program as param
+version.update(program);
 
-data = JSON.parse(fs.readFileSync('package.json'));
-
-clog.info('Current version is', data.version);
-
-if (program.set != null) {
-  data.version = program.set;
-  clog.ok('Version set to', data.version);
-  
-} else {
-  if (data.version == null) {
-    clog.error('Error: please set an initial version using --set');
+// do git stuff
+currentRepo = git('.');
+currentRepo.add([".",], undefined, true);
+currentRepo.commit(program.message, function(err, output) {
+  if(err) {
+    clog.error(err);
     process.exit(1);
-    
-  } else {
-    
-    version = data.version.split('.');
-    
-    if (program.major != null) {
-      version[0] = parseInt(version[0], 10) + 1;
-      version[1] = 0;
-      version[2] = 0;
-    } else {
-      if (program.minor != null) {
-        version[1] = parseInt(version[1], 10) + 1;
-        version[2] = 0;
-      } else {
-        version[2] = parseInt(version[2], 10) + 1;
-      }
-    }
-    
-    
-    data.version = version.join('.');
-    clog.ok('Version set to', data.version);
   }
-}
+  
+  clog.info("Pushing commit");
+  currentRepo.push("origin", "master", function(err, success) {
+    if(err) {
+      clog.error(err);
+      process.exit(1);
+    } else {
+      clog.ok("Push done", success);
+    }
+  });
 
-fs.writeFileSync('package.json', JSON.stringify(data, null, 2));
+});
+
+
+
+
